9c664c0ee09ac63d782b8a1579b301fe86116b8f
palomalo <malo.pavol@gmail.com>
2020-05-23 13:53:34 +0200

    Service Consumer for CurrencyConverter
    Implemented with and RpcResponse object with response values in its properties.

--- CurrencyConverter_RpcClient_.NET-RabbitMQ/RPC_client/RpcResponse.cs
+++ CurrencyConverter_RpcClient_.NET-RabbitMQ/RPC_client/RpcResponse.cs
@@ -5,10 +5,11 @@ using System.Text;
 namespace RpcClientCCY
 {
     //TODO: include this class in the microservice communicating with the currency converter.
-    public class RpcRequest
+    public class RpcResponse
     {
         public string fromCCY { get; set; }
         public string toCCY { get; set; }
-        
+
+        public double exchangeRate { get; set; }
     }
 }
