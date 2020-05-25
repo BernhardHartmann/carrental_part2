61724a1da195d0c7a66cccf18af1adfb63d18c78
basharw <basharwattar@gmail.com>
2020-05-25 11:38:02 +0200

    aufraümen

--- RabbitResCar/CarManagement/CarManagement/CarManagementConsumer/CarReceiver.cs
+++ RabbitResCar/CarManagement/CarManagement/CarManagementConsumer/CarReceiver.cs
@@ -96,49 +96,8 @@ namespace CarManagement
             }
             catch(Exception ex)
             {
-
-            }
-           
-
-            Console.WriteLine($"Consuming Message");
-            //Console.WriteLine(string.Concat("Message received from the exchange ", exchange));
-            //Console.WriteLine(string.Concat("Consumer tag: ", consumerTag));
-            //Console.WriteLine(string.Concat("Delivery tag: ", deliveryTag));
-            //Console.WriteLine(string.Concat("Routing tag: ", routingKey));
-            //Console.WriteLine(string.Concat("Message: ", Encoding.UTF8.GetString(body)));
-
-
-            //if (exchange.Equals("request.cars"))
-            //{
-            //    if (routingKey.Equals("get_random_car"))
-            //    {
-
-            //var locationAndCat = JsonConvert.DeserializeObject<Object>(Encoding.UTF8.GetString(body));
-            //var Location = ((Newtonsoft.Json.Linq.JContainer)locationAndCat).First;
-            //var LocationID = ((Newtonsoft.Json.Linq.JProperty)Location).Value;
-            //var category = ((Newtonsoft.Json.Linq.JContainer)locationAndCat).Last;
-            //var categoryID = ((Newtonsoft.Json.Linq.JProperty)Location).Value;
-
-            //        carservice = new CarService();
-
-            //        string randomCarJSON = carservice.get_CarBy_Category_And_Location((int)LocationID, (int)categoryID);
-
-
-            //        Console.WriteLine(string.Concat("desirialized: ", locationAndCat));
-
-            //        System.Threading.Thread.Sleep(3000);
-
-            //        /*
-            //         * DB get 
-            //         */
-
-            //        /* DirectMessageBackToCarReq ds = new DirectMessageBackToCarReq();
-            //         ds.SendMessageToReservation(carConsumed);  */
-            //    }
-            //    Console.WriteLine("retrieved car DATA: " + body);
-            //    _channel.BasicAck(deliveryTag, false);
-
-            //}
+                _channel.BasicNack(deliveryTag, true, true);
+            }        
         }
     }
 }
